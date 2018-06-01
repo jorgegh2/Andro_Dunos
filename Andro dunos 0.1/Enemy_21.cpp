3dc8f8738923f8323b30a9ef96df37cc8dbf8c7d
Jorge <jgherencia@gmail.com>
2018-06-02 01:07:42 +0200

    fix animation of enemy 21

--- Andro dunos 0.1/Enemy_21.cpp
+++ Andro dunos 0.1/Enemy_21.cpp
@@ -5,16 +5,19 @@
 
 Enemy_21::Enemy_21(int x, int y) : Enemy(x, y)
 {
+	//fly.speed = 0.1f;
+	fly.PushBack({ 164, 111, 44, 26 });  //1
+	fly.PushBack({ 106, 51, 44, 26 });	//2
+	fly.PushBack({ 48, 111, 44, 26 }); //3
+	fly.PushBack({ 164, 51, 44, 26 }); //4
+	fly.PushBack({ 106, 111, 44, 26 });//5
+	fly.PushBack({ 48, 51, 44, 26 }); //6
 
-	fly.PushBack({ 48, 51, 44, 26 });
-	fly.PushBack({ 106, 51, 44, 26 });
-	fly.PushBack({ 164, 51, 44, 26 });
-	fly.PushBack({ 48, 111, 44, 26 });
-	fly.PushBack({ 106, 111, 44, 26 });
-	fly.PushBack({ 164, 111, 44, 26 });
-	fly.PushBack({ 56, 171, 44, 26 });
-	fly.PushBack({ 110, 111, 44, 26 });
-	fly.PushBack({ 164, 111, 44, 26 });
+	//when its hurt
+	//fly.PushBack({ 56, 171, 44, 26 });
+	//fly.PushBack({ 110, 111, 44, 26 });
+	//fly.PushBack({ 164, 111, 44, 26 });
+	fly.speed = 0.2f;
 	fly.loop = true;
 
 	animation = &fly;
