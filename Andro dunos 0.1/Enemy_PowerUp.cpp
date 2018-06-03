a1e7b22974f05aec47370379290c7697e3799845
Nadine Gutiérrez <36292059+Nadine044@users.noreply.github.com>
2018-06-03 13:08:23 +0200

    Enemy Power Up Movement Coord

--- Andro dunos 0.1/Enemy_PowerUp.cpp
+++ Andro dunos 0.1/Enemy_PowerUp.cpp
@@ -18,6 +18,30 @@ Enemy_Power_Up::Enemy_Power_Up(int x, int y) : Enemy(x, y)
 	fly.PushBack({ 0,5,18,12 });*/
 
 	fly.PushBack({ 4,0,18,26 }); //initial
+	
+	/*giro1Abajo.PushBack({55, 0, 18, 25});
+	giro1Abajo.PushBack({ 82, 0, 18, 25 }); //mover hacia adelante
+
+	giro1Arriba.PushBack({134, 0, 18, 26});
+	giro1Arriba.PushBack({ 160, 0, 18, 26 });
+	giro1Arriba.PushBack({ 186, 0, 18, 26 });
+	giro1Arriba.PushBack({ 212, 0, 18, 26 }); //movimiento diagonal
+	
+	Recula.PushBack({ 212, 0, 18, 26 });
+	Recula.PushBack({ 263, 2, 21, 21 });
+	Recula.PushBack({ 1, 30, 25, 18 });
+	Recula.PushBack({ 26, 31, 26, 18 });
+	Recula.PushBack({ 53, 31, 25, 18 });
+	Recula.PushBack({ 79, 31, 25, 18 });
+	Recula.PushBack({ 107, 31, 21, 21 });
+	Recula.PushBack({ 132, 31, 21, 21 });
+	Recula.PushBack({ 161, 27, 18, 25 });
+	Recula.PushBack({ 185, 27, 18, 25 });
+	Recula.PushBack({ 212, 26, 18, 26 });
+	Recula.PushBack({ 263, 26, 19, 26 });
+	Recula.PushBack({ 4, 52, 18, 26 });
+	Recula.PushBack({ 30, 52, 18, 26 });
+	Recula.PushBack({ 56, 52, 18, 26 }); //movimiento hacia la derecha*/
 
 
 
